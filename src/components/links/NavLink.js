82da741c2d783179669830a0c37fd491f36d1b4a
timhwang21 <timhwang21@gmail.com>
2017-10-15 14:39:58 -0400

    NavLink> add support for index routes

--- src/components/links/NavLink.js
+++ src/components/links/NavLink.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { bool } from 'prop-types';
 import { NavLink as RouterNavLink } from 'react-router-dom';
 
 import { hidable } from '../decorators';
 
 import './NavLink.css';
 
-const NavLink = props => (
+const isActive = (match, location) => match || location.pathname === '/';
+
+const NavLink = ({ index, ...props }) => (
   <RouterNavLink
     {...props}
     exact
     className="NavLink"
     activeClassName="NavLink-active"
+    isActive={index ? isActive : undefined}
   />
 );
+
+NavLink.propTypes = {
+  index: bool,
+};
 
 NavLink.displayName = 'NavLink';
 
