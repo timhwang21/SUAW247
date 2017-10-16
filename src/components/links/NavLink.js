7c523d65f7a7b7c531d852e644dc4c8f9c46a47e
timhwang21 <timhwang21@gmail.com>
2017-10-15 21:09:40 -0400

    [feature] Only enable 'Now' tab in work mode
    * 'Today' tab enabled in break mode
    * 'Week' tab permanently disabled until it has at least mock content

--- src/components/links/NavLink.js
+++ src/components/links/NavLink.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { bool } from 'prop-types';
+import React, { Component } from 'react';
+import { string, bool } from 'prop-types';
+import classnames from 'classnames';
 import { NavLink as RouterNavLink } from 'react-router-dom';
 
 import { hidable } from '../decorators';
@@ -8,20 +9,43 @@ import './NavLink.css';
 
 const isActive = (match, location) => match || location.pathname === '/';
 
-const NavLink = ({ index, ...props }) => (
-  <RouterNavLink
-    {...props}
-    exact
-    className="NavLink"
-    activeClassName="NavLink-active"
-    isActive={index ? isActive : undefined}
-  />
-);
+class NavLink extends Component {
+  static propTypes = {
+    className: string,
+    disabled: bool,
+    index: bool,
+  };
 
-NavLink.propTypes = {
-  index: bool,
-};
+  handleClick = e => {
+    const { disabled } = this.props;
 
-NavLink.displayName = 'NavLink';
+    disabled && e.preventDefault();
+  };
+
+  get className() {
+    const { className, disabled } = this.props;
+
+    return classnames({
+      NavLink: true,
+      [className]: className,
+      disabled,
+    });
+  }
+
+  render() {
+    const { index, ...props } = this.props;
+
+    return (
+      <RouterNavLink
+        {...props}
+        exact
+        className={this.className}
+        activeClassName="NavLink-active"
+        isActive={index ? isActive : undefined}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
 
 export default hidable(NavLink);
--- src/features/Home/components/Dashboard/Dashboard.js
+++ src/features/Home/components/Dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { bool } from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { Route, withRouter } from 'react-router-dom';
 
+import { getIsBreak } from '../../../../modules/clock';
 import { LogInPage } from '../../../../components/auth';
 import { AuthenticatedRoute } from '../../../../components/routes';
 import { Panel, NavBar } from '../../../../components/layout';
@@ -12,14 +16,22 @@ import Week from './components/Week';
 
 import './Dashboard.css';
 
-const Dashboard = () => (
+const mapStateToProps = state => ({
+  isBreak: getIsBreak(state),
+});
+
+const Dashboard = ({ isBreak }) => (
   <Panel withNav id="Dashboard">
     <NavBar id="Dashboard-NavBar">
       <NavLink to="/now" index>
         Now
       </NavLink>
-      <NavLink to="/today">Today</NavLink>
-      <NavLink to="/week">This Week</NavLink>
+      <NavLink to="/today" disabled={!isBreak}>
+        Today
+      </NavLink>
+      <NavLink to="/week" disabled>
+        This Week
+      </NavLink>
     </NavBar>
     <AuthenticatedRoute exact path="/(now)?" component={Now} />
     <AuthenticatedRoute path="/today" component={Today} />
@@ -28,4 +40,10 @@ const Dashboard = () => (
   </Panel>
 );
 
-export default withRouter(Dashboard);
+Dashboard.propTypes = {
+  isBreak: bool,
+};
+
+Dashboard.displayName = 'Dashboard';
+
+export default compose(withRouter, connect(mapStateToProps))(Dashboard);
