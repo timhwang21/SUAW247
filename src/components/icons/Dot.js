528e512cdefcb6282a4e0ae896b8830782b0e631
timhwang21 <timhwang21@gmail.com>
2017-10-20 10:49:15 -0400

    Add <Dot/> icon

---
+++ src/components/icons/Dot.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Dot.css';
+
+const Dot = () => <i className="icon-dot">{'\u2219'}</i>;
+
+export default Dot;
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,4 +1,5 @@
 export { default as Chevron } from './Chevron';
 export { default as Desktop } from './Desktop';
 export { default as Expand } from './Expand';
+export { default as Dot } from './Dot';
 export { default as Star } from './Star';
