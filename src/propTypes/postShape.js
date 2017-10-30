77e3dd43c0bbc59c6978bad55f83c5901a886bc2
timhwang21 <timhwang21@gmail.com>
2017-10-29 20:03:17 -0400

    [minor] Allow dates in Post propType

--- src/propTypes/postShape.js
+++ src/propTypes/postShape.js
@@ -1,10 +1,10 @@
-import { shape, string, number } from 'prop-types';
+import { oneOfType, shape, string, number, instanceOf } from 'prop-types';
 
 export default shape({
   id: string,
   user_id: string,
-  created_at: string,
-  updated_at: string,
+  created_at: oneOfType([string, instanceOf(Date)]),
+  updated_at: oneOfType([string, instanceOf(Date)]),
   goal: string,
   accomplishment: string,
   productivity: number,
